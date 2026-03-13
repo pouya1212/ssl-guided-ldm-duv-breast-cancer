cf61147d1bcf4add9ab5261b292f68f23cc4fb0e
pouya1212 <35433017+pouya1212@users.noreply.github.com>
2026-03-13 00:08:24 -0400

    Update configs.py

--- models/configs.py
+++ models/configs.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 # Copyright 2020 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,6 +12,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+"""
+Configuration definitions for various Vision Transformer (ViT) architectures,
+including Base, Large, Huge, and Hybrid (ResNet + ViT) variants.
+"""
 
 import ml_collections
 
