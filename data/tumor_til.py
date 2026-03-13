306e8d00b4852c77a7b97cd7a19e8cca29a9c711
pouya1212 <35433017+pouya1212@users.noreply.github.com>
2026-03-12 23:48:22 -0400

    Create tumor_til.py

---
+++ data/tumor_til.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+import h5py
+import numpy as np
+from torch.utils.data import Dataset, random_split, DataLoader, WeightedRandomSampler
+from PIL import Image
+import io
+import torch
+from torchvision import transforms as T
+import pandas as pd
+import os
+import random
+import torch.nn as nn
+from sklearn.model_selection import train_test_split
+import random
+from pathlib import Path
+import pandas as pd
+import torch
+from torch.utils.data import Dataset
+from torchvision import transforms as T
+from PIL import Image
+import random
+import torch.nn as nn
+from sklearn.model_selection import train_test_split
+from PIL import Image
+import h5py
+from torch.utils.data import Dataset  # just for Dataset base class
+from pathlib import Path
+import h5py
+
+
+
